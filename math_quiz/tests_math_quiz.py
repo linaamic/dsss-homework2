3f608227f473768fb27a9fe52c6a73ba9e373dc2
linaamic <alina.schlabritz@hotmail.de>
2023-11-13 18:13:17 +0100

    added unit tests

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_int, random_math_operation, generate_problem_and_answer
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,32 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = random_int(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        random_operation = random_math_operation()
+        # Test if operator returns either +, - or *
+        self.assertTrue(random_operation == "*" or random_operation == "+" or random_operation == "-")
+        # Test if operator is of type str
+        self.assertTrue(type(random_operation) == str)
+        
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (1, 4, '*', '1 * 4', 4),
+                (2, 4, '*', '2 * 4', 8),
+                (5, 5, '*', '5 * 5', 25),
+                (3, 6, '-', '3 - 6', -3),
+                (6, 3, '-', '6 - 3', 3),
             ]
-
+            # for all test cases check whether problem and answer are as expected
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                problem, answer = generate_problem_and_answer(num1, num2, operator)
+                self.assertTrue(problem == expected_problem)
+                self.assertTrue(answer == expected_answer)
+                
 
 if __name__ == "__main__":
     unittest.main()
